6f7767e48a3e6c008fe48b9d569d7103902bd0e1
root <menno@mhamburg.nl>
2020-11-29 16:21:43 +0000

    Sun Nov 29 2020 - Committed via Codeherent

--- main.tf
+++ main.tf
@@ -1,47 +1,49 @@
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_resource_group" "rg_landingpage" {
-  name     = "rg-landingpage-we-${var.env}"
+  name = "rg-landingpage-we-${var.env}"
   location = "West Europe"
 }
 
 resource "azurerm_key_vault" "example" {
-  name                        = "kv-landingpage-we-${var.env}"
-  location                    = azurerm_resource_group.rg_landingpage.location
-  resource_group_name         = azurerm_resource_group.rg_landingpage.name
+  name = "kv-landingpage-we-${var.env}"
+  location = azurerm_resource_group.rg_landingpage.location
+  resource_group_name = azurerm_resource_group.rg_landingpage.name
   enabled_for_disk_encryption = true
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  soft_delete_enabled         = true
-  soft_delete_retention_days  = 7
-  purge_protection_enabled    = false
-
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  soft_delete_enabled = true
+  soft_delete_retention_days = 7
+  purge_protection_enabled = false
   sku_name = "standard"
-
   access_policy {
     tenant_id = data.azurerm_client_config.current.tenant_id
     object_id = data.azurerm_client_config.current.object_id
-
     secret_permissions = [
       "get",
       "set",
       "list",
-      "delete"
+      "delete",
     ]
   }
-
   network_acls {
     default_action = "Allow"
-    bypass         = "AzureServices"
+    bypass = "AzureServices"
   }
-
   contact {
     email = "example@example.com"
-    name  = "example"
+    name = "example"
     phone = "0123456789"
   }
-
   tags = {
     environment = var.env
     application = "landingpage"
   }
+}
+
+resource "azurerm_storage_account" "storage_account" {
+  resource_group_name = azurerm_resource_group.rg_landingpage
+  name = 
+  account_replication_type = 
+  account_tier = 
+  location = azurerm_resource_group.rg_landingpage
 }
